2aa934c8bd4d500e68fa17f6fd95a0a6b6abe38b
joj-cju <dnjswls2019@naver.com>
2026-03-24 10:55:03 +0900

    Refactor: 변수명, 파일명 통일
    - 모든 이미지의 이름을 변수명과 통일하였습니다.
    - 일부 상수를 제외한 변수들을 카멜케이스로 변경하였습니다.
    - 메서드의 어순을 동사가 앞에오도록 변경하였습니다.
    - 일부 상수(MAX_EXP 등)는 가독성을 위해 변경하지 않았습니다.

--- frontend/src/game/game1/scenes/GameScene.js
+++ frontend/src/game/game1/scenes/GameScene.js
@@ -11,35 +11,35 @@ export default class GameScene extends Phaser.Scene {
     // 이미지에는 모두 언더바를 넣는것으로 하겠습니다(가독성을 위해)
 
     // 플레이어 이미지
-    this.load.image("player_stop", "/assets/game1/Player/stop.png");
+    this.load.image("player_stop", "/assets/game1/Player/player_stop.png");
 
     // 플레이어 이동 이미지
-    this.load.image("player_move1", "/assets/game1/Player/move1.png");
-    this.load.image("player_move2", "/assets/game1/Player/move2.png");
-    this.load.image("player_move3", "/assets/game1/Player/move3.png");
+    this.load.image("player_move1", "/assets/game1/Player/player_move1.png");
+    this.load.image("player_move2", "/assets/game1/Player/player_move2.png");
+    this.load.image("player_move3", "/assets/game1/Player/player_move3.png");
 
     // 공격 이미지
-    this.load.image("blade_1", "/assets/game1/Attacks/Blade/1.png");
-    this.load.image("blade_2", "/assets/game1/Attacks/Blade/2.png");
-    this.load.image("blade_3", "/assets/game1/Attacks/Blade/3.png");
+    this.load.image("blade_1", "/assets/game1/Attacks/Blade/blade_1.png");
+    this.load.image("blade_2", "/assets/game1/Attacks/Blade/blade_2.png");
+    this.load.image("blade_3", "/assets/game1/Attacks/Blade/blade_3.png");
 
     // 몬스터 이미지
-    this.load.image("slime_stop", "/assets/game1/Monster/Nomal/slime.png"); // 슬라임(몬스터1)
+    this.load.image("slime_stop", "/assets/game1/Monster/Nomal/slime_stop.png"); // 슬라임(몬스터1)
 
     // 타일
-    this.load.image("map1_tile1", "/assets/game1/Tile/map1/tile1.png");
+    this.load.image("map1_tile1", "/assets/game1/Tile/map1/map1_tile1.png");
 
     // 공통으로 사용하는 UI바 배경
-    this.load.image("Bar_BackGround", "/assets/game1/Ui/Bar_BackGround.png");
+    this.load.image("bar_background", "/assets/game1/Ui/bar_background.png");
 
     // HP바
-    this.load.image("hpBar", "/assets/game1/Ui/hpBar.png");
+    this.load.image("hp_bar", "/assets/game1/Ui/hp_bar.png");
 
     // 경험치 구슬
-    this.load.image("expBall", "/assets/game1/Object/Exp/exp.png");
+    this.load.image("exp_ball", "/assets/game1/Object/Exp/exp_ball.png");
 
     // 경험치바
-    this.load.image("expBar", "/assets/game1/Ui/expBar.png");
+    this.load.image("exp_bar", "/assets/game1/Ui/exp_bar.png");
   }
 
   create() { // create: 말그대로 생성, 오브젝트를 작성하는 곳
@@ -62,13 +62,13 @@ export default class GameScene extends Phaser.Scene {
     // ======================HP=====================
 
     // 배경
-    this.hpBar = this.add.nineslice(0, 0, 'Bar_BackGround', 0, 8, 4, 1, 1, 1, 1);
+    this.hpBar = this.add.nineslice(0, 0, 'bar_background', 0, 8, 4, 1, 1, 1, 1);
     this.hpBar.width = 20;
     this.hpBar.setDepth(100); // 레이어 우선순위(높을수록 우선)
     this.hpBar.setScale(2); // setScale: 해상도 조정, N배만큼 키워준다
 
     // 실제 차오르는 체력
-    this.addHPValue = this.add.nineslice(0, 0, 'hpBar', 0, 8, 4, 1, 1, 1, 1);
+    this.addHPValue = this.add.nineslice(0, 0, 'hp_bar', 0, 8, 4, 1, 1, 1, 1);
     this.addHPValue.width = 20;
     this.addHPValue.setDepth(101);
     this.addHPValue.setOrigin(0, 0.5);
@@ -80,23 +80,23 @@ export default class GameScene extends Phaser.Scene {
     // ====================경험치====================
 
     // 경험치바 위치
-    const EXP_BAR_POS_X = this.cameras.main.width / 2;
-    const EXP_BAR_POS_Y = this.cameras.main.height / 15;
+    const expBarPosX = this.cameras.main.width / 2;
+    const expBarPosY = this.cameras.main.height / 15;
 
     // 차오르는 경험치바 한정 위치
-    const ADD_EXP_POS_X = EXP_BAR_POS_X - 150;
+    const addExpPosX = expBarPosX - 150;
 
     // 경험치 바 배경(expBar)
     // nieslice는 상하좌우 n픽셀은 건들지않고 크기를 조정할 수 있다.
     // (x,y, 텍스쳐이름, 프레임, 가로, 세로, 보호픽셀 좌,우,위,아래)
-    this.expBar = this.add.nineslice(EXP_BAR_POS_X, EXP_BAR_POS_Y, 'Bar_BackGround', 0, 16, 8, 1, 1, 1, 1); 
+    this.expBar = this.add.nineslice(expBarPosX, expBarPosY, 'bar_background', 0, 16, 8, 1, 1, 1, 1); 
     this.expBar.setScrollFactor(0); // 카메라를 따라오도록 설정한다
     this.expBar.width = 150; // 배경UI 넓이 설정
     this.expBar.setDepth(100); // 레이어 우선순위(높을수록 우선)
     this.expBar.setScale(2); // setScale: 해상도 조정, N배만큼 키워준다
 
     // 경험치
-    this.addExpValue = this.add.nineslice(ADD_EXP_POS_X, EXP_BAR_POS_Y, 'expBar', 0, 16, 8, 1, 1, 1, 1); 
+    this.addExpValue = this.add.nineslice(addExpPosX, expBarPosY, 'exp_bar', 0, 16, 8, 1, 1, 1, 1); 
     this.addExpValue.setOrigin(0, 0.5); // 왼쪽에서 오른쪽으로 늘어나게 한다
     this.addExpValue.setScrollFactor(0); // 카메라를 따라오도록 설정한다
     this.addExpValue.setDepth(101); // 해당 스프라이트를 최상단 레이어에 놓는다
@@ -132,7 +132,7 @@ export default class GameScene extends Phaser.Scene {
 
       if (player.isDamage) return; // 피해 입는중이면 패스
 
-      this.hpRecount(monster.damage);
+      this.updateHP(monster.damage);
 
       player.isDamage = true;
       player.setTint(0xff0000); // 피격 이펙트(붉은색)
@@ -178,7 +178,7 @@ export default class GameScene extends Phaser.Scene {
     // 몬스터생성 이벤트
     this.time.addEvent({
       delay: 2000,
-      callback: this.monsterSpawn,
+      callback: this.spawnMonster,
       callbackScope: this,
       loop: true,
     })
@@ -257,16 +257,16 @@ export default class GameScene extends Phaser.Scene {
   }
 
   // 몬스터 스폰 클래스(기존의 반복문을 대체)
-  monsterSpawn() {
+  spawnMonster() {
 
     // 생성범위
     const SPAWN_RADIUS = 400;
     // Between을 통해 랜덤한 각도를 뽑아낸다
-    const RANDOM_ANGLE = Phaser.Math.FloatBetween(0, Math.PI * 2);
+    const randomAngle = Phaser.Math.FloatBetween(0, Math.PI * 2);
 
     // 플레이어의 현재 위치와 비교하여 원의 테두리 좌표에 몬스터가 스폰될 위치를 정합니다
-    const SPAWN_X = this.player.x + Math.cos(RANDOM_ANGLE) * SPAWN_RADIUS;
-    const SPAWN_Y = this.player.y + Math.sin(RANDOM_ANGLE) * SPAWN_RADIUS;
+    const SPAWN_X = this.player.x + Math.cos(randomAngle) * SPAWN_RADIUS;
+    const SPAWN_Y = this.player.y + Math.sin(randomAngle) * SPAWN_RADIUS;
 
     // 설명: 플레이어를 기준으로 반지름이 400인 원을 그린뒤 그 원의 테두리를 기준으로 랜덤하게 몬스터를 소환한다
 
@@ -279,8 +279,8 @@ export default class GameScene extends Phaser.Scene {
   }
 
   // 경험치 구슬 생성
-  expBallAdd(POS_X, POS_Y) {
-    const expBall = this.expBalls.create(POS_X, POS_Y, "expBall");
+  addExpBall(posX, posY) {
+    const expBall = this.expBalls.create(posX, posY, "exp_ball");
     expBall.setScale(2);
   }
 
@@ -306,14 +306,14 @@ export default class GameScene extends Phaser.Scene {
 
       const percent = this.expCount / this.MAX_EXP; //퍼센트 계산
 
-      const EXP_BAR_PERCENT_VALUE = Math.max(2, 150 * percent);// 2: setScale 즉 처음 생성될때부터 이미지 크기 정해놓는것
+      const expBarPercentValue = Math.max(2, 150 * percent);// 2: setScale 즉 처음 생성될때부터 이미지 크기 정해놓는것
 																													     // 150 * percent인 이유: 150이 기본 크기여서 150의 n%로 하면 딱 맞는다  
-      this.addExpValue.width = EXP_BAR_PERCENT_VALUE; // 계산 완료된걸 넣어준다
+      this.addExpValue.width = expBarPercentValue; // 계산 완료된걸 넣어준다
     }
   }
 
   // hp가 변화됐을때 적용되는 클래스, 기본적으로 EXP와 작동방식이 같다
-  hpRecount(Value) {
+  updateHP(Value) {
 
     this.player.hp += Value;
 
@@ -332,8 +332,8 @@ export default class GameScene extends Phaser.Scene {
     else {
       const percent = this.player.hp / this.MAX_HP;
 
-      const HP_BAR_PERCENT_VALUE = Math.max(2, 20 * percent);
-      this.addHPValue.width = HP_BAR_PERCENT_VALUE
+      const hpBarPercentValue = Math.max(2, 20 * percent);
+      this.addHPValue.width = hpBarPercentValue
     }
   }
 
@@ -341,8 +341,8 @@ export default class GameScene extends Phaser.Scene {
   autoAttack() {
 
     // 플레이어의 위치를 받고
-    const POS_X = this.player.x;
-    const POS_Y = this.player.y;
+    const posX = this.player.x;
+    const posY = this.player.y;
 
     // 플레이어의 방향을 받는다
     const isLeft = this.player.flipX;
@@ -352,7 +352,7 @@ export default class GameScene extends Phaser.Scene {
 
     // 이펙트 생성
     // 기본공격(블레이드)
-    const atkEff = this.physics.add.sprite(POS_X + OFFSET_X, POS_Y, "blade_1");
+    const atkEff = this.physics.add.sprite(posX + OFFSET_X, posY, "blade_1");
     atkEff.setScale(4);
     atkEff.setFlipX(isLeft);
     atkEff.play("blade_animation");
@@ -383,7 +383,7 @@ export default class GameScene extends Phaser.Scene {
 
       // 죽었으면~
       if (monster.hp <= 0) {
-        this.expBallAdd(monster.x, monster.y);
+        this.addExpBall(monster.x, monster.y);
         monster.destroy(); // 체력이 다 달면 없애기
       }
 
