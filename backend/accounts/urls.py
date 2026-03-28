d7d3cae60608657e54ef0aa4c0224955701aa290
LeviLinth <levilinth@gmail.com>
2026-03-28 18:17:53 +0900

    FEAT: 회원탈퇴 기능 추가

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import CheckEmailView, SignupView, LoginView, LogoutView
+from .views import CheckEmailView, SignupView, LoginView, LogoutView, WithdrawalView
 
 # 인증 관련 URL 패턴
 # /api/v1/auth/ 하위 경로는 config/urls.py에서 include로 연결됨
@@ -8,4 +8,5 @@ urlpatterns = [
     path("signup", SignupView.as_view(), name="signup"),                # 일반 회원가입
     path("login", LoginView.as_view(), name="login"),                   # 일반 로그인
     path("logout", LogoutView.as_view(), name="logout"),                # 일반 로그아웃
+    path("withdrawal", WithdrawalView.as_view(), name='withdrawal'),    # 일반 회원탈퇴
 ]
