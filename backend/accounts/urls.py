11206164b03bfabde1e6184d0f3c2d5d6d7fb115
LeviLinth <levilinth@gmail.com>
2026-04-03 12:00:01 +0900

    FEAT: 비밀번호 재설정 링크 전송 API
    현재 무료 버전 Supabase에서 이메일 전송 테스트는 하루 3번만 할 수 있습니다.
    때문에 Resend는 하루 100번 이메일 전송이 가능하여 테스트만 Resend로 할 예정
    입니다.
    
    현실에 존재하지 않는 이메일을 작성하면 500에러가 뜹니다 때문에 실제 사용하는
    본인 이메일로 테스트를 하셔야 합니다.
    먼저 Resend를 회원가입 해주세요. Resend를 회원가입한 이메일로 회원가입 API를
    사용해서 회원가입을 해야합니다. (회원가입을 하고 Supabase의 왼쪽
    Authentication탭에 가셔서 회원가입이 되었는지 확인할 수 있습니다.)
    그리고 방금 가입한 이메일로 이번 비밀번호 재설정 링크 전송 API를 테스트
    해주시면 됩니다. (저는 처음에 스팸 메일함에 있었습니다. 메일이 오지 않는다면
    스팸 메일함 확인해 주세요.)
    
    아직 비밀번호를 재설정하는 API는 구현 되어있지 않습니다. 또한 받으시는 재설정
    링크도 현재 구현되어있지 않기 때문에 빈 페이지로 접속됩니다.

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,15 +1,16 @@
 from django.urls import path
-from .views import CheckEmailView, SignupView, LoginView, LogoutView, ChangePasswordView, WithdrawalView, ChangeUsernameView, UserImageView
+from .views import CheckEmailView, SignupView, LoginView, LogoutView, ChangePasswordView, WithdrawalView, ChangeUsernameView, UserImageView, ResetPasswordView
 
 # 인증 관련 URL 패턴
 # /api/v1/auth/ 하위 경로는 config/urls.py에서 include로 연결됨
 urlpatterns = [
-    path("check-email", CheckEmailView.as_view(), name="check-email"),      # 이메일 중복 확인
-    path("signup", SignupView.as_view(), name="signup"),                    # 일반 회원가입
-    path("login", LoginView.as_view(), name="login"),                       # 일반 로그인
-    path("logout", LogoutView.as_view(), name="logout"),                    # 일반 로그아웃
-    path("password", ChangePasswordView.as_view(), name="change-password"), # 비밀번호 변경
-    path("withdrawal", WithdrawalView.as_view(), name='withdrawal'),        # 일반 회원탈퇴
-    path("username", ChangeUsernameView.as_view(), name="change-username"), # 유저 이름 변경
-    path("userimage", UserImageView.as_view(), name="userimage"),           # 프로필 사진 변경 / 기본으로 변경
+    path("check-email", CheckEmailView.as_view(), name="check-email"),          # 이메일 중복 확인
+    path("signup", SignupView.as_view(), name="signup"),                        # 일반 회원가입
+    path("login", LoginView.as_view(), name="login"),                           # 일반 로그인
+    path("logout", LogoutView.as_view(), name="logout"),                        # 일반 로그아웃
+    path("password", ChangePasswordView.as_view(), name="change-password"),     # 비밀번호 변경
+    path("password/reset", ResetPasswordView.as_view(), name="reset-password"), # 비밀번호 재설정
+    path("withdrawal", WithdrawalView.as_view(), name='withdrawal'),            # 일반 회원탈퇴
+    path("username", ChangeUsernameView.as_view(), name="change-username"),     # 유저 이름 변경
+    path("userimage", UserImageView.as_view(), name="userimage"),               # 프로필 사진 변경 / 기본으로 변경
 ]
