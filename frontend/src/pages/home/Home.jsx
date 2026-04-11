6a9c676a5d93986b0647b39c35d561bf9c4af053
somin16 <daisy20031001@gmail.com>
2026-04-11 23:35:50 +0900

    Refactor : Home.jsx 리팩토링 (불필요한 코드 삭제&useTheme적용)

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,13 +1,12 @@
-import React from "react";
-import { Link } from 'react-router-dom';
 import { getAssetUrl } from "../../utils/assetHelper";
+import { useTheme } from "../../theme_states/useTheme";
 
-function Home() {
+export default function Home() {
   // 현재 테마 상태
-  const currentTheme = "winter_light"; 
+  const currentTheme = useTheme((state) => state.currentTheme) 
 
   // 인라인 스타일로 배경 이미지를 동적으로 적용
-  const containerStyle = {
+  const backgroundStyle = {
     backgroundImage: `url(${getAssetUrl(currentTheme,'backgrounds', 'background_x3')})`,
     backgroundSize: '100% 100%',
     backgroundRepeat: 'no-repeat',
@@ -15,14 +14,8 @@ function Home() {
     width: '100%',
     position: 'relative'
   };
-
   return (
-    /* 1. index.css에서 정의한 .page-container 클래스를 사용합니다. */
-    /* 2. 배경 이미지는 style 속성으로 넘겨줍니다. */
-    <div className="page-container" style={containerStyle}>
-
-    </div>
+    <div style={backgroundStyle}/>
   );
 }
 
-export default Home;
