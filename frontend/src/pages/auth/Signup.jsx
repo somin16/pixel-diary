dea755e26438f7da304e9c654477b641f2b61343
somin16 <daisy20031001@gmail.com>
2026-04-18 02:49:05 +0900

    Refactor : auth/components적용 Signup.jsx
    회원가입 페이지에 auth/components를 적용했습니다 + 주석 수정 및 추가
    
    1. 입력창
    2. sumbit버튼
    
    오타 없는지 확인,
    서버 실행 후 이미지 누락 없이 회원가입 화면이 잘 보이는지만 확인 및 캡쳐 해주시면 됩니당
    
    > 입력창 좌상단에 설명 잘 보이는지
    > 입력창에 값을 입력했을 떄 하단에 피드백 메시지가 잘 나오는지
    
    주소뒤에 /signup 붙여서 접속
    
    백엔드 서버를 안 키면 회원가입은 불가능합니다
    Related #123

--- frontend/src/pages/auth/Signup.jsx
+++ frontend/src/pages/auth/Signup.jsx
@@ -1,15 +1,24 @@
+// 1. 리액트 불러오기
 import { useNavigate } from 'react-router-dom';
-
-import { getAssetUrl } from "../../utils/AssetHelper";
-import { useTheme } from "../../hooks/useTheme";
-
-import { supabase } from "../../utils/SupabaseClient";
 import { useState, useEffect } from 'react';
 
+// 2. 유틸 함수 불러오기
+import { getAssetUrl } from "../../utils/AssetHelper";
 import AuthValidator from '../../utils/AuthValidator';
+
+// 3. 커스텀 훅 불러오기
+import { useTheme } from "../../hooks/useTheme";
 import useDebounce from '../../hooks/useDebounce';
 
-export default function signup() {
+// 4. 슈파베이스 불러오기
+import { supabase } from "../../utils/SupabaseClient";
+
+// 5. 컴포넌트 불러오기
+import InputBox from '../../components/auth/InputBox';
+import SubmitButton from '../../components/auth/SubmitButton';
+
+export default function signup() { // 회원가입 페이지 내보내기
+    // 페이지 이동
     const navigate = useNavigate();
 
     // 현재 테마
@@ -22,7 +31,7 @@ export default function signup() {
     const [confirmPassword, setConfirmPassword] = useState('');
 
     // [상태] 회원가입 중인지 아닌지
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(false); // 기본값 false
 
     // 디바운스 적용 (서버 요청 횟수 조절)
     const debouncedUserEmail = useDebounce(user_email, 500) // 0.5초 딜레이
@@ -34,7 +43,7 @@ export default function signup() {
     const [passwordStatus, setPasswordStatus] = useState({ state: 'default', message: '' });
     const [confirmStatus, setConfirmStatus] = useState({ state: 'default', message: '' });
 
-    // 이메일 : 형식 + 중복 체크 (true)를 전달해 로그인 페이지와는 달리 중복체크 가능하게함
+    // 유효성 검사 이메일 : 두 번째 인자를 true로 전달해 로그인 페이지와는 달리 중복검사 가능하게함
     useEffect( () => {
         const checkEmail = async () => {
             if (debouncedUserEmail) {
@@ -45,7 +54,7 @@ export default function signup() {
         checkEmail();
     }, [debouncedUserEmail]);
 
-    // 닉네임: 길이 + 중복 체크
+    // 유효성 검사 닉네임: 길이 + 중복 체크
   useEffect(() => {
     const checkUserName = async () => {
       if (debouncedUserName) {
@@ -56,7 +65,7 @@ export default function signup() {
     checkUserName();
   }, [debouncedUserName]);
 
-  // 비밀번호: 규칙 체크
+  // 유효성 검사 비밀번호: 형식
   useEffect(() => {
     setPasswordStatus(AuthValidator.validatePassword(password));
   }, [password]);
@@ -67,7 +76,7 @@ export default function signup() {
   }, [password, confirmPassword]);
 
 
-    // 일반 이메일 회원가입 로직
+    // 일반 회원가입 로직
     const onSignupSubmit = async (e) => {
         e.preventDefault();
 
@@ -78,13 +87,15 @@ export default function signup() {
             passwordStatus.state === 'success' &&
             confirmStatus.state === 'success';
 
+        // 최종 확인 : 에러가 있거나 빈값이면 중단
         if (!isAllValid) {
             alert("모든 항목을 올바르게 입력해주세요");
             return;
         }
 
         setLoading(true);
-        try{
+
+        try{ //백엔드 API 연동 /api/v1/auth/signup/
             const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/signup/`, {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
@@ -106,21 +117,6 @@ export default function signup() {
         }
     }
 
-    // 스타일 적용
-    // 회원가입 인풋창 배경 이미지 적용 스타일
-    const signInputBoxStyle = {
-        backgroundImage: `url(${getAssetUrl(currentTheme, 'boxes', 'auth_info_input_box_x3')})`,
-        backgroundSize: '100% 100%'
-    }
-    // 회원가입 인풋창 공통 클래스 네임
-    const signupInputClassName = "w-full h-full p-6 bg-transparent outline-none placeholder:text-gray-400 text-sm font-bold text-center mt-2 -mb-0.5";
-
-    // 회원가입 버튼 배경 이미지 적용 스타일
-    const signupButtonStyle = {
-        backgroundImage: `url(${getAssetUrl(currentTheme, 'buttons', 'auth_submit_button_x3')})`,
-        backgroundSize: '100% 100%'
-    };
-
     return(
         // 전체 컨테이너
         <div className='w-full h-full items-center flex flex-col p-25'>
@@ -131,91 +127,56 @@ export default function signup() {
             <form onSubmit={onSignupSubmit} noValidate className="w-full flex flex-col gap-2 mb-5">
                 
                 {/* 닉네임 입력창 */}
-                <div className='w-full' style={signInputBoxStyle}>
-                    <span className='absolute mt-3 ml-4 font-bold text-xs'>닉네임</span>
-                    <input
-                        type='username'
-                        placeholder = "닉네임을 입력하세요"
-                        className = {signupInputClassName}
-                        value={user_name}
-                        onChange={(e) => setUser_name(e.target.value)}
-                    />
-                </div>
-
-                {/* 닉네임 피드백 메시지 추가 */}
-                <div className="h-3 flex items-center pl-2">
-                    <span className={`text-xs pl-2 font-bold ${AuthValidator.STATUS_COLORS[userNameStatus?.state]}`}>
-                        {userNameStatus.message}    
-                    </span>            
-                </div>                
+                <InputBox // auth/InputBox 컴포넌트를 불러와서 사용
+                    label="닉네임"
+                    type="username"
+                    placeholder="닉네임을 입력하세요"
+                    value={user_name}
+                    onChange={(e) => setUser_name(e.target.value)}
+                    status={userNameStatus}
+                    currentTheme={currentTheme}
+                />               
 
                 {/* 이메일 입력창 */}
-                <div className='w-full' style={signInputBoxStyle}>
-                    <span className='absolute mt-3 ml-4 font-bold text-xs'>이메일</span>
-                    <input
-                        type='useremail'
-                        placeholder = "이메일을 입력하세요"
-                        className = {signupInputClassName}
-                        value={user_email}
-                        onChange={(e) => setUser_email(e.target.value)}
-                    />
-                </div>
-
-                {/* 이메일 피드백 메시지 추가 */}
-                <div className="h-3 flex items-center pl-2">
-                    <span className={`text-xs pl-2 font-bold ${AuthValidator.STATUS_COLORS[emailStatus?.state]}`}>
-                        {emailStatus.message}    
-                    </span>            
-                </div> 
+                <InputBox // auth/InputBox 컴포넌트를 불러와서 사용
+                    label="이메일"
+                    type="email"
+                    placeholder="이메일을 입력하세요"
+                    value={user_email}
+                    onChange={(e) => setUser_email(e.target.value)}
+                    status={emailStatus}
+                    currentTheme={currentTheme}
+                />
 
                 {/* 비밀번호 입력창 */}
-                <div className='w-full' style={signInputBoxStyle}>
-                    <span className='absolute mt-3 ml-4 font-bold text-xs'>비밀번호</span>
-                    <input
-                        type='password'
-                        placeholder = "비밀번호를 입력하세요"
-                        className = {signupInputClassName}
-                        value={password}
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
-                </div>
-
-                {/* 비밀번호 피드백 메시지 추가 */}
-                <div className="h-3 flex items-center pl-2">
-                    <span className={`text-xs pl-2 font-bold ${AuthValidator.STATUS_COLORS[passwordStatus.state]}`}>
-                        {passwordStatus.message}    
-                    </span>            
-                </div> 
+                <InputBox // auth/InputBox 컴포넌트를 불러와서 사용
+                    label="비밀번호"
+                    type="password"
+                    placeholder="비밀번호를 입력하세요"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    status={passwordStatus}
+                    currentTheme={currentTheme}
+                />
 
                 {/* 비밀번호 확인창 */}
-                <div className='w-full' style={signInputBoxStyle}>
-                    <span className='absolute mt-3 ml-4 font-bold text-xs'>비밀번호 확인</span>
-                    <input
-                        type='confrimpassword'
-                        placeholder = "비밀번호를 한번 더 입력하세요"
-                        className = {signupInputClassName}
-                        value={confirmPassword}
-                        onChange={(e) => setConfirmPassword(e.target.value)}
-                    />
-                </div>
-
-                {/* 비밀번호 확인 피드백 메시지 추가 */}
-                <div className="h-3 flex items-center pl-2">
-                    <span className={`text-xs pl-2 font-bold ${AuthValidator.STATUS_COLORS[confirmStatus.state]}`}>
-                        {confirmStatus.message}    
-                    </span>            
-                </div> 
+                <InputBox // auth/InputBox 컴포넌트를 불러와서 사용
+                    label="비밀번호 확인"
+                    type="confrimpassword"
+                    placeholder="비밀번호를 한번 더 입력하세요"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                    status={confirmStatus}
+                    currentTheme={currentTheme}
+                />
 
                 {/* 회원가입 버튼 */}
-                <button
-                    disabled={loading || userNameStatus.state !== 'success' || emailStatus.state !== 'success' || passwordStatus.state !== 'success' || confirmStatus.state !== 'success'} // 회원가입 중에는 클릭 방지
-                    type='submit'
-                    className=
-                        {`w-full p-5 text-white font-bold text-2xl transition-transform outline-none
-                        ${(loading || userNameStatus.state !== 'success' || emailStatus.state !== 'success' || passwordStatus.state !== 'success' || confirmStatus.state !== 'success') ? 'opacity-50' : 'active:scale-95'}`}
-                    style={signupButtonStyle}>
-                    {loading ? "회원가입 중" : "회원가입"}
-                </button>
+                <SubmitButton // auth/SubmitButton 컴포넌트 불러와서 사용
+                    loading={loading}
+                    disabled={emailStatus.state !== 'success' || passwordStatus.state !== 'success'}
+                    currentTheme={currentTheme}
+                    text="회원가입"
+                />
             </form>
         </div>
     )
