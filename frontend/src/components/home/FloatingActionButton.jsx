7dd908c6b1f30e1f6edee9b04140f470a71e2bf2
somin16 <daisy20031001@gmail.com>
2026-04-18 11:25:43 +0900

    Feat : 플로팅액션버튼 <일기 추가(작성)> 컴포넌트 추가
    - 코드와 주석에 오타가 없는지만 확인해주시면 됩니다
    - **테스트 불가X**
    
    - 우선 1학기에는 네비게이션 탭을 4개로 하고 홈화면에서 추가 버튼을 우 하단에
    띄우기로 했습니다
    
    - 일기 추가(작성) 버튼 컴포넌트입니다

---
+++ frontend/src/components/home/FloatingActionButton.jsx
@@ -0,0 +1,23 @@
+import { getAssetUrl } from "../../utils/AssetHelper"
+
+// home/FloatingActionButton FAB버튼 컴포넌트 선언
+const FloatingActionButton = ({onClick, currentTheme, ariaLabel = "일기작성버튼"}) => {
+
+    const FloatingActionButtonStyle = {// 픽셀 아트 배경 이미지 & 비율 설정
+        backgroundImage: `url(${getAssetUrl(currentTheme, 'icons', 'add_icon_x3')})`,
+        backgroundSize: '100% 100%',// 이미지를 박스 크기에 꽉 채움
+        aspectRatio: '1/1'// 비율을 유지해 픽셀 왜곡 방지
+    };
+    
+    return(
+        <button
+            className="absolute w-19 h-auto bottom-30 right-6 outline-none" // absolute는 부모요소 내에서 포함 (부모가 position: relative;를 가지고 있어야 정상작동)
+            onClick={onClick}
+            aria-label={ariaLabel} // 눈에 보이지 않는 텍스트 이름표 (화면 낭독기가 어떤 버튼인지 음성으로 읽어줌)기본값: 일기작성버튼)
+            style={FloatingActionButtonStyle} // 텍스트가 없는 닫기X / 추가+ / 뒤로가기 > 같은 버튼에는 aria-label 속성 부여
+        />
+    )
+    
+}
+
+export default FloatingActionButton
