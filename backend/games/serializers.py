875f55feda7a7f1da4105bc651411a4e50db7508
hyunbin-dot <joh9201@gmail.com>
2026-04-25 21:07:16 +0900

    Fix: GameScoreSerializer 중복 선언 제거
    제가 이해를 잘못해서 중복 선언되는 코드를 안지우고 커밋을 올렷습니다.
    시리얼라이저 확인 해주시면 됩니다.
    
    serializers 파일에 AddUserCoinSerializer 확인 해주시면 됩니다. views 파일에 AddUserCoinView 확인 해주시면됩니다.
    urls 파일에 from games.views import AddUserCoinView
    path("users/coins/add/", AddUserCoinView.as_view(), name="add-user-coin" ),추가햇습니다
    "참고사항" 브루노 Docs에 추가 했습니다.
    
    테스트방법
    backend에서 서버 켜주시고 브루노 들어가 주세요.
    로그인들어가서 로그인 하신 후 나온 엑세스 코드 복사후
    Headers에 bearer 뒤에 엑세스 코드 넣고 실행시 200번 확인해주시면 됩니다.
    
    400 에러 헤더에 bearer뒤에 비워두고 샌드 하시면 나옵니다.
    
    401에러 헤더가셔서 bearer뒤에 영문자 아무거나 쓰신후 401
    유효하지 않은 토큰입니다 나오는지 확인 해주시면 됩니다.
    
    404에러 테스트는 이 방식만 가능 영상추가
    참고사항: "user_id는 복사하셔서 메모장에 넣어야 복구 가능합니다."
    1.슈퍼베이스들어가셔서 table editor-> users 들어간 후 자신의 유저아이디삭제 2.로그인 하셔서 엑세스코드 베이저 뒤에 넣어주세요.
    3.엑세스코드 베이저 뒤에 넣엇으면 샌드 누르시면 404 에러 발생합니다.
    
    복구방법
    상단의 insert -> insertrow 누르신 후 복사해놓은 유저아이디 넣은 후
    coin이나 랭킹은 숫자 아무거나 넣으셔도 됩니다.
    user_name은 전에 쓰시던 이름 넣으시면 됩니다.

--- backend/games/serializers.py
+++ backend/games/serializers.py
@@ -1,18 +1,6 @@
 # Django REST Framework의 시리얼라이저 모듈
 from rest_framework import serializers
 
-class GameScoreSerializer(serializers.Serializer):
-    # game_score 필드 - 정수형, 기본값 null
-    game_score = serializers.IntegerField(
-        required=False,   # 키가 없어도 됨
-        default=0,     # 기본값은 0
-        min_value=0,      # 0 이상이어야 함
-        error_messages={
-            "invalid": "game_score는 숫자여야 합니다.",
-            "min_value": "game_score는 0 이상이어야 합니다.",
-        }
-    )
-
 # 공통 game_score 필드를 가진 베이스 시리얼라이저
 class GameBaseScoreSerializer(serializers.Serializer):
     game_score = serializers.IntegerField(
