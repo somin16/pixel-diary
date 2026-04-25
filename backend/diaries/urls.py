f93e1252fff99c6f210dca8570a71d072ce60ae8
LeviLinth <levilinth@gmail.com>
2026-04-25 20:38:40 +0900

    Refactor: PR 리뷰 내용 반영
    일기 삭제 url 경로에 '/' 빠진 것을 확인하고 브루노와 diaries/urls.py에
    추가했습니다.
    (추가하는 과정에서 일기 테이블에 FK가 적용되어 있지 않다는 것을 발견하고 카톡에
    공지했습니다.)
    
    기존 PR의 아이템 구매 및 보관함 조회에서 두가지의 url로 작동하는 문제를 config/
    urls.py와 store/urls.py를 수정해서 해결했습니다.

--- backend/diaries/urls.py
+++ backend/diaries/urls.py
@@ -5,5 +5,5 @@ from .views import DiaryView, DiaryDetailView
 # /api/v1/diaries/ 하위 경로는 config/urls.py에서 include로 연결됨
 urlpatterns = [
     path("", DiaryView.as_view(), name="diaries"),                           # 일기 작성 및 저장
-    path("<int:diary_id>", DiaryDetailView.as_view(), name="diary-detail"),  # 일기 수정/삭제
+    path("<int:diary_id>/", DiaryDetailView.as_view(), name="diary-detail"),  # 일기 수정/삭제
 ]
