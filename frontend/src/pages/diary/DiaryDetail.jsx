d73071c3c58d9c18b38b4b4a5fe2a3d14cbfa958
somin16 <daisy20031001@gmail.com>
2026-04-28 00:13:48 +0900

    Feat: 일기 상세 보기 화면 구현
    - 현재 일기 상세 조회API는 미구현이므로 연동X, 더미 데이터를 넣어놨습니다
    
    - 테스트 진행하실 때 이미 작성된 일기가 1개라도 있어야지 일기 목록에 갤러리처럼 뜹니다
    - 일기 최소 1개 작성하신 후 테스트 진행하세요
    - 해당 갤러리 아이템을 클릭하시면 일기 상세 보기 페이지로 이동이 됩니다
    (url 예: localhost/diary/2026-04-28 )
    
    - [ ] 제가 올린 사진과 일치하는지 ( 화면 해상도에 따른 약간의 비율 달라짐은 괜찮습니다)
    - [ ] 오른쪽 상단의 ••• 을 눌렀을 때 메뉴가 잘 뜨는지
    - [ ] 오타 확인

--- frontend/src/pages/diary/DiaryDetail.jsx
+++ frontend/src/pages/diary/DiaryDetail.jsx
@@ -1,3 +1,5 @@
+import { useParams } from "react-router-dom";
+import DetailDiaryDialog from "../../components/diary/DetailDiaryDialog";
 import { useTheme } from "../../hooks/useTheme";
 import { getAssetUrl } from "../../utils/AssetHelper";
 
@@ -6,15 +8,32 @@ export default function DiaryDetail(){
     // 현재 테마 상태
     const currentTheme = useTheme((state) => state.currentTheme);
     
+    const { date } = useParams(); // URL에서 날짜를 가져옴
+
+    // 더미 데이터 설정
+    const dummyDiary = {
+        date: date ? date.replace(/-/g, '. ') : "26. 04. 26", // 2026. 04. 26 형식으로 변환
+
+        content: "오늘은 펭귄들과 함께 일기 앱을 만들었다. 에러 때문에 고생했지만 열심히 해결했다! 내일은 더 열심히 만들어야지. ❤️🩷🧡💛💚💙🩵💜🤎🖤🩶🤍💔❤️‍🔥❤️‍🩹❣️💕💞💓💗💖💘💝",
+    };
+
     return(
         <div
-            className="w-full h-full"
+            className="relative w-full h-full"
             style={{
                 backgroundImage: `url(${getAssetUrl(currentTheme, 'backgrounds','background_x3' )})`,
                 backgroundSize: '100% 100%'
             }}
         >
-        </div>
-        
+            <div className="absolute inset-0 bg-black/60 backdrop-blur-xs z-1"/>
+
+            <DetailDiaryDialog
+                currentTheme={currentTheme}
+                mode="view"
+                date={dummyDiary.date}
+                imageUrl=""
+                content={dummyDiary.content}
+            />
+        </div> 
     );
 };
