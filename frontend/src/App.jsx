346d98e5db97a23e04557e15404a7d9f9753d82b
somin16 <daisy20031001@gmail.com>
2026-05-11 14:21:30 +0900

    feat: 공지사항 상제조회, 작성/수정 화면 구현 및 API 연동
    - 공지사항 상세조회 페이지
    - 공지사항 작성 / 수정 페이지
    
    - 카테고리를 미선택시 기본은 '공지' 카테고리로 들어갑니다
    - 카테고리 버튼, 카테고리 입력 으로 선택 가능합니다
    - 공지사항 작성, 수정, 삭제, 상세조회 API 모두 연동한 상황입니다
    - 작성/수정 시 제목과 내용이 누락되면 토스트메시지가 하단에 뜨면서 "제목과 내용은 필수입니다"라는 안내메시지가 뜹니다
    
    - 영상 테스트가 좋을 것 같아요
    - 관리자 전용 페이지다 보니 일부는 픽셀이미지가 아닙니다 (굳이 픽셀로 할 필요성을 못느꼈습니다)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -25,7 +25,10 @@ import Profile from "./pages/more/profile/Profile"; // 더보기 - 프로필 화
 import Setting from "./pages/more/setting/Setting"; // 더보기 - 설정 메인화면
 import Shop from "./pages/more/shop/Shop"; // 더보기 - 상점 화면
 import Inventory from "./pages/more/inventory/Inventory"; // 더보기 - 보관함 화면
+// ---------------------- 더보기 (공지사항) -----------------------------
 import AnnouncementList from "./pages/more/announcement/AnnouncementList"; // 더보기 - 공지사항 목록 화면
+import AnnouncementDetail from "./pages/more/announcement/AnnouncementDetail"; // 더보기 - 공지사항 상세 조회 화면
+import AnnouncementForm from "./pages/more/announcement/AnnouncementForm"; // 더보기 - 공지사항 작성/수정 화면
 
 // BrowserRouter 안에서 훅을 호출하는 내부 컴포넌트
 function AppInner() {
@@ -84,10 +87,10 @@ function AppInner() {
                         {/* 주소가 / 이면 홈화면을 보여줘 */}
                         <Route path="/" element={<Home />} />
 
-                        {/* 주소가 /diary/list 이면 일기 목록 화면을 보여줘 */}
+                        {/* 주소가 /diary/list 이면 일기 목록 조회 화면을 보여줘 */}
                         <Route path="/diary/list" element={<ListDiary />} />
 
-                        {/* 주소가 /diary/:date 이면 일기 상세 보기 화면을 보여줘 */}
+                        {/* 주소가 /diary/:date 이면 일기 상세 조회 화면을 보여줘 */}
                         <Route path="/diary/:date" element={<DiaryDetail />} />
 
                         {/* 주소가 /diary/write/:date 이면 일기 작성 화면을 보여줘 */}
@@ -112,6 +115,14 @@ function AppInner() {
 
                         {/* 주소가 /more/announcement/list이면 공지사항 목록 화면을 보여줘 */}
                         <Route path="/more/announcement/list" element={<AnnouncementList />} />
+
+                        {/* 주소가 /more/announcement/detail이면 공지사항 상세 조회 화면을 보여줘 */}
+                        <Route path="/more/announcement/detail/:announcement_id" element={<AnnouncementDetail />} />
+
+                        {/* 주소가 /more/announcement/write이면 공지사항 작성 화면을 보여줘 */}
+                        <Route path="/more/announcement/write" element={<AnnouncementForm />} />
+                        {/* 주소가 /more/announcement/edit/:announcement_id이면 공지사항 수정 화면을 보여줘 */}
+                        <Route path="/more/announcement/edit/:announcement_id" element={<AnnouncementForm />} />
 
                         {/* ⚠️ 이미 로그인했는데 로그인창 가려하면 홈으로 보내기 */}
                         <Route path="/auth/*" element={<Navigate to="/" replace />} />
