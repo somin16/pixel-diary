897bf84ddf4c17a1673944be7e74f6912b84855e
somin16 <daisy20031001@gmail.com>
2026-05-15 17:49:44 +0900

    오타수정 default Update AssetHelper.js

--- frontend/src/utils/AssetHelper.js
+++ frontend/src/utils/AssetHelper.js
@@ -7,18 +7,18 @@
  * @returns {string} - 모바일 앱 환경에서도 깨지지 않는 이미지 경로
  */
 export const getAssetUrl = (currentThemeName, assetFolderName, fileName) => {
-    const DEFALUT_THEME_NAME = 'winter_light'; // 기본 테마 나중에 변경 예정
+    const DEFAULT_THEME_NAME = 'winter_light'; // 기본 테마 나중에 변경 예정
 
     try {
-        const targetThemeName = currentThemeName || DEFALUT_THEME_NAME;
+        const targetThemeName = currentThemeName || DEFAULT_THEME_NAME;
 
         // Public 폴더 기준 상대 경로
         return `/assets/theme/${targetThemeName}/${assetFolderName}/${fileName}.png`;
 
     } catch (error) { // 현재 테마에서 에셋을 찾는 도중 에러 발생, 경로 구조상 문제가 생기면 자동으로 기본테마 경로 반환
-        console.warn(`[에셋]: ${currentThemeName}에 ${fileName}이 없어 기본테마인 ${DEFALUT_THEME_NAME}로 대체합니다.`);
+        console.warn(`[에셋]: ${currentThemeName}에 ${fileName}이 없어 기본테마인 ${DEFAULT_THEME_NAME}로 대체합니다.`);
 
-        return `/assets/theme/${DEFALUT_THEME_NAME}/${assetFolderName}/${fileName}.png`;
+        return `/assets/theme/${DEFAULT_THEME_NAME}/${assetFolderName}/${fileName}.png`;
     }
 };
 
